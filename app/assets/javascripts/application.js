3f2c11fd79e4d621fefcc640eea6aeb991f02e29
Mitsukiooka <66833185+Mitsukiooka@users.noreply.github.com>
2021-03-21 19:49:00 +0900

    design install

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -2,3 +2,11 @@
 //= require popper
 //= require bootstrap-sprockets
 //= require rails-ujs
+//= require jquery/jquery.min
+//= require bootstrap/js/bootstrap.bundle.min
+//= require jquery.easing/jquery.easing.min
+//= require php-email-form/validate
+//= require aos/aos
+//= require isotope-layout/isotope.pkgd.min
+//= require owl.carousel/owl.carousel.min
+//= require main
