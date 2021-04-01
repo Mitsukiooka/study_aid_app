0efa7244849fce8aeed8832d072231d030ed08e9
Mitsukiooka <66833185+Mitsukiooka@users.noreply.github.com>
2021-04-01 14:19:16 +0900

    add locales ja

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,13 @@ module StudyAidApp
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
 
+    config.active_record.default_timezone = :local
+    config.time_zone = 'Tokyo'
+    config.i18n.default_locale = :ja
+    config.i18n.available_locales = [:ja, :en]
+    config.i18n.fallbacks = true
+
+    config.encoding = "utf-8"
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
