faa0479bf9cee9659500c1544e65d48c3a80514e
Mitsukiooka <66833185+Mitsukiooka@users.noreply.github.com>
2021-04-02 12:05:26 +0900

    add remind mail

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -5,4 +5,10 @@ class NotificationMailer < ApplicationMailer
     @user = user
     mail to: @user.email, from: "mooka0907@gmail.com", subject: "学習計画 #{@plan.title}が作成されました！"
   end
+
+  def remind_mail(plan, user)
+    @plan = plan
+    @user = user
+    mail to: @user.email, from: "mooka0907@gmail.com", subject: "学習計画 #{@plan.title}の期間が終了しました！"
+  end
 end
